20dd290feec7fe429f1321130e26716d459f9cfd
rweilwww <github@shaw-weil.com>
2025-10-18 21:13:15 -0400

    from inDesign 2025-10-19 01:11

---
+++ gitpush.bat
@@ -0,0 +1,18 @@
+
+cd /inetpub/wwwroot/w/mcclintock
+git init
+            git config --global --add safe.directory C:/inetpub/wwwroot/w/mcclintock
+   git remote add origin https://github.com/rweilwww/web-mcclintock.git
+   git remote -v
+
+        
+
+git add .
+git commit -m "from inDesign 2025-10-19 01:11" --no-edit
+git push --force --set-upstream origin main
+
+rem wait for one minute to allow git hub to get its act together
+timeout 60
+rem go fo it!!
+timeout 5
+exit
